4ed984de86a2225b0f0d4c9108cfa7dbd14ec393
sifische <sifische@ucsc.edu>
2019-07-08 11:08:55 -0700

    Fixed some issues with Profile Button, now able to select Picture from downloads

--- RunAndTrack/app/src/main/java/com/example/runandtrack/Profile.java
+++ RunAndTrack/app/src/main/java/com/example/runandtrack/Profile.java
@@ -1,75 +1,81 @@
 package com.example.runandtrack;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.app.Activity;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.content.Intent;
+import android.widget.Toast;
 
 public class Profile extends AppCompatActivity {
+
+    //===================Story 2===============================//
+    private static int IMG_RESULT = 1001;
+    String ImageDecode;
+    ImageView imageViewLoad;
+    Button LoadImage;
+    Intent intent;
+    String[] FILE;
+    //=========================================================//
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        //Story 2 - Initializing Button
 
-        mImageView = findViewById(R.id.Profile_Picture);
-        mChooseBtn = findViewById(R.id.Edit_Profile_Picture_Button);
+        //===================Story 2===============================//
+        imageViewLoad = (ImageView) findViewById(R.id.Profile_Picture);
+        LoadImage = (Button)findViewById(R.id.Edit_Profile_Picture_Button);
 
-        //handle button click
-        mChooseBtn.setOnClickListener(new View.OnClickListener() {
+        LoadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-                        //permission not granted so we request
-                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-                        requestPermissions(permissions, PERMISSION_CODE);
-                    } else { //permission granted
-                        pickImageFromGallery();
-                    }
-                } else {
-                    //permission already granted
-                }
+                intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, IMG_RESULT);
             }
         });
 
-        private void pickImageFromGallery(){
-            //intent to pick image
-            Intent intent = new Intent(Intent.ACTION_PICK);
-            intent.setType("image/*");
-            startActivity(intent,IMAGE_PICK_CODE);
-        }
-        @Override
-        protected void onActivityResult(int requestCode,int resultCode,Intent data){
-            if(resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE){
-                //set image to image view
-                mImageView.setImageURI(data.getData());
-            }
-        }
-        @Override
-        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,@NonNull int[] grantResults){
-            //super.onRequestPermissionsResult(requestCode,permissions,grantResults);
-            switch(requestCode){
-                case PERMISSION_CODE:
-                    if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                        //permission was granted
-                        pickImageFromGallery();
-                    }
-                    else{//permission denied
-                        Toast.makeText(this, "Permission denied",Toast.LENGTH_SHORT).show();
-                    }
-            }
-        }
-        //End of Story 2
+        //=========================================================//
         Button ProfileHomeButton = (Button)findViewById(R.id.ProfileHomeButton);
         ProfileHomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        try{
+            if(requestCode == IMG_RESULT && resultCode == RESULT_OK && null != data) {
+                Uri URI = data.getData();
+                String[] FILE = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(URI, FILE, null, null, null);
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(FILE[0]);
+                ImageDecode = cursor.getString(columnIndex);
+                cursor.close();
+
+                imageViewLoad.setImageBitmap(BitmapFactory.decodeFile(ImageDecode));
+            }
+        }catch(Exception e){
+            Toast.makeText(this,"Please try again",Toast.LENGTH_LONG).show();
+        }
     }
 }
