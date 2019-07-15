4cb410ba444e7186b0d0c4c7358c95423dc85056
sifische <sifische@ucsc.edu>
2019-07-14 20:05:57 -0700

    Finished the Profile Functions

--- RunAndTrack/app/src/main/java/com/example/runandtrack/Profile.java
+++ RunAndTrack/app/src/main/java/com/example/runandtrack/Profile.java
@@ -2,8 +2,10 @@ package com.example.runandtrack;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -29,6 +31,8 @@ import java.io.InputStream;
 public class Profile extends AppCompatActivity {
     public static final int Image_Gallery_Request = 20;
     private ImageView Profile_Picture;
+    SharedPreferences sh;
+    EditText editWeight, editName, editAge, editSex;
 
     //This method will be invoked when the user clicks the edit button
     public void onImageGalleryClicked(View v){
@@ -54,6 +58,25 @@ public class Profile extends AppCompatActivity {
 
         Profile_Picture = (ImageView) findViewById(R.id.Profile_Picture);
 
+        sh = getSharedPreferences("runProfile", MODE_PRIVATE);
+        editWeight = findViewById(R.id.profileWeight);
+        editName = findViewById(R.id.profileName);
+        editAge = findViewById(R.id.profileAge);
+        editSex = findViewById(R.id.profileSex);
+
+        Float weight = sh.getFloat("profileWeight", -1);
+        int age = sh.getInt("profileAge", -1);
+        String name = sh.getString("profileName", "");
+        String sex = sh.getString("profileSex", "");
+
+        if (weight != -1) {
+            editWeight.setText(Float.toString(weight));
+        }
+        if (age != -1) {
+            editAge.setText(Integer.toString(age));
+        }
+        editName.setText(name);
+        editSex.setText(sex);
         Button ProfileHomeButton = (Button)findViewById(R.id.ProfileHomeButton);
         ProfileHomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,6 +100,7 @@ public class Profile extends AppCompatActivity {
                     inputStream = getContentResolver().openInputStream(imageUri);
                     Bitmap imagebtmp = BitmapFactory.decodeStream(inputStream);       //Puts stream data into bitmap format
                     Profile_Picture.setImageBitmap(imagebtmp);                        //Loads Image to ImageView
+
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                     //error message if image is unavailable
@@ -84,5 +108,33 @@ public class Profile extends AppCompatActivity {
                 }
             }
         }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor edit = sh.edit();
+        if (!editWeight.getText().toString().isEmpty())
+            edit.putFloat("profileWeight", Float.parseFloat(editWeight.getText().toString()));
+        if (!editAge.getText().toString().isEmpty())
+            edit.putInt("profileAge", Integer.parseInt(editAge.getText().toString()));
+        edit.putString("profileName", editName.getText().toString());
+        edit.putString("profileSex", editSex.getText().toString());
+
+        Bitmap bm = ((BitmapDrawable)Profile_Picture.getDrawable()).getBitmap();
+        //Profile_Picture.setImageBitmap(bm);
+        edit.apply();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Bitmap bm = ((BitmapDrawable)Profile_Picture.getDrawable()).getBitmap();
+        outState.putParcelable("Profile_Bitmap",bm);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Bitmap bm = savedInstanceState.getParcelable("Profile_Bitmap");
+        Profile_Picture.setImageBitmap(bm);
     }
 }
